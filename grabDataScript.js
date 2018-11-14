73511697367848d924b9f4be4d3f45299df5ba0d
Aditya Bist <adbist@microsoft.com>
2018-11-14 11:14:31 -0800

    added file for scripting data updates

---
+++ grabDataScript.js
@@ -0,0 +1,30 @@
+var admin = require("firebase-admin");
+var fs = require("fs");
+var schedule = require('node-schedule');
+
+var serviceAccount = require("./accountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+db.settings({ timestampsInSnapshots: true });
+
+
+db.collection("entries").orderBy("timestamp", "desc").limit(720).get().then(querySnapshot => {
+  let data = querySnapshot.docs;
+
+  data = data.map(dataPoint => dataPoint.data());
+
+  let str = "window.vsIssueTrackerDataPoints = " + JSON.stringify(data);
+
+  fs.writeFile("./../ads-issue-tracker/data.js", str, (error) => {
+      if (error) {
+        console.error(error);
+        return;
+      };
+  });
+  console.log(str);
+});
