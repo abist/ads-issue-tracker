c87e31fbe37f057d95e19afe0d5ed445b6b21d90
Aditya Bist <adbist@microsoft.com>
2018-11-13 17:30:09 -0800

    added scheduled job to pull data

--- grabData.js
+++ grabData.js
@@ -1,5 +1,6 @@
 var admin = require("firebase-admin");
 var fs = require("fs");
+var schedule = require('node-schedule');
 
 var serviceAccount = require("./accountKey.json");
 
@@ -11,19 +12,20 @@ let db = admin.firestore();
 
 db.settings({ timestampsInSnapshots: true });
 
-db.collection("entries").orderBy("timestamp", "desc").limit(720).get().then(querySnapshot => {
-  let data = querySnapshot.docs;
-
-  data = data.map(dataPoint => dataPoint.data());
-
-  let str = "window.vsIssueTrackerDataPoints = " + JSON.stringify(data);
-
-  fs.writeFile("./data.js", str, (error) => {
-      if (error) {
-        console.error(error);
-        return;
-      };
-  })
-  console.log(str);
+schedule.scheduleJob('5 * * * *', function() {
+  db.collection("entries").orderBy("timestamp", "desc").limit(720).get().then(querySnapshot => {
+    let data = querySnapshot.docs;
+  
+    data = data.map(dataPoint => dataPoint.data());
+  
+    let str = "window.vsIssueTrackerDataPoints = " + JSON.stringify(data);
+  
+    fs.writeFile("./data.js", str, (error) => {
+        if (error) {
+          console.error(error);
+          return;
+        };
+    });
+    console.log(str);
+  });
 });
-
